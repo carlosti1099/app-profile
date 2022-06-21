3576d16e9651a983c28abbe3d9600923da6916ce
carlosti1099 <carlos.ti1099@gmail.com>
2022-06-21 17:36:19 -0400

    Adicionado Linking

--- project-profile/App.js
+++ project-profile/App.js
@@ -1,5 +1,5 @@
 
-import { StyleSheet, Image, View, SafeAreaView, StatusBar, Text } from 'react-native';
+import { StyleSheet, Image, View, SafeAreaView, StatusBar, Text, Pressable, Linking } from 'react-native';
 
 const colorGithub = '#010409';
 const colorFontGithub = '#C9D1D9';
@@ -7,7 +7,19 @@ const colorDarkFontGithub = '#4F565E';
 
 const imageProfileGithub = 'https://avatars.githubusercontent.com/u/66273229?v=4';
 
+const urlToMyGithub = 'https://github.com/carlosti1099';
+
 export default function App() {
+
+  const handlePressGoToGithub = async () => {
+    console.log('Verificando link');
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if(res){
+      console.log('link aprovado');
+      console.log('Abrindo link...');
+      await Linking.openURL(urlToMyGithub);
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
@@ -17,11 +29,27 @@ export default function App() {
           style={styles.avatar}
           source={{uri: imageProfileGithub}}
         />
-        <Text style={[styles.defaultText, styles.name]}>Carlos Roberto</Text>
-        <Text style={[styles.defaultText, styles.nickname]}>carlosti1099</Text>
-        <Text style={[styles.defaultText, styles.description]}>
+        <Text
+          accessibilityLabel='Nome: carlos roberto'
+          style={[styles.defaultText, styles.name]}>
+          Carlos Roberto
+        </Text>
+        <Text
+          accessibilityLabel='Nickname: carlosti1099'
+          style={[styles.defaultText, styles.nickname]}>
+          carlosti1099
+        </Text>
+        <Text
+          accessibilityLabel='Descrição: Engenheiro Computacional | 
+          Suporte Técnico em Ti | Front-End Developer'
+          style={[styles.defaultText, styles.description]}>
           Engenheiro Computacional | Suporte Técnico em Ti | Front-End Developer
         </Text>
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={styles.button}>
+            <Text style={[styles.defaultText, styles.textButton]}>Open in Github</Text>
+          </View>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -63,5 +91,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 14,
   },
+  button: {
+    marginTop: 20,
+    backgroundColor: colorDarkFontGithub,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
   
 });
