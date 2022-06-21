1c08e883b9eabdfdcc4c63b807d8397a4ed1673e
carlosti1099 <carlos.ti1099@gmail.com>
2022-06-21 15:43:05 -0400

    Trabalhando com flex-box e adicionando name, nickname e descricao

--- project-profile/App.js
+++ project-profile/App.js
@@ -1,15 +1,27 @@
 
-import { StyleSheet, Image, View, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, Image, View, SafeAreaView, StatusBar, Text } from 'react-native';
 
 const colorGithub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorDarkFontGithub = '#4F565E';
+
 const imageProfileGithub = 'https://avatars.githubusercontent.com/u/66273229?v=4';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
-      <View >
-        <Image accessibilityLabel='Carlos no escritório' style={styles.avatar} source={{uri: imageProfileGithub}}/>
+      <View style={styles.contente}>
+        <Image 
+          accessibilityLabel='Carlos no escritório'
+          style={styles.avatar}
+          source={{uri: imageProfileGithub}}
+        />
+        <Text style={[styles.defaultText, styles.name]}>Carlos Roberto</Text>
+        <Text style={[styles.defaultText, styles.nickname]}>carlosti1099</Text>
+        <Text style={[styles.defaultText, styles.description]}>
+          Engenheiro Computacional | Suporte Técnico em Ti | Front-End Developer
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -17,8 +29,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    // column
     backgroundColor: colorGithub,
     flex: 1, //expandi para a tela inteira
+    justifyContent: 'center',
+    alignItems: 'center',
+    // flexDirection: 'row',
+  },
+  contente: {
+    alignItems: 'center',
+    padding: 20,
   },
   avatar: {
     height: 200,
@@ -26,6 +46,22 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     borderColor: 'red',
     borderWidth: 2,
+  },
+  defaultText: {
+    color: colorFontGithub,
+  },
+  name: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  nickname: {
+    fontSize: 18,
+    color: colorDarkFontGithub,
+  },
+  description: {
+    fontWeight: 'bold',
+    fontSize: 14,
   },
   
 });
